c27177b07710d942105d8789529244cb8ea6882f
Jeff <jeff@jeffpuckett.com>
2015-09-14 20:28:11 -0500

    Task cleanup code, add comments.

--- src/Controller.h
+++ src/Controller.h
@@ -69,13 +69,15 @@ public:
 	void onChar(char c)
 	{
 		if(c == 's'){
+			// skip to part right before death
 			std::cout << "skipping " << (*ghighScore - m_model.score) << " pipes..\n";
 			skip = true;
 			*ssleep = false;
 			return;
 		}
 		if(c == 'a'){
-			
+			// toggle the q-agent on/off
+			// q-table will continue to update
 			if(agentPlay){
 				agentPlay = false;
 				std::cout << "Disabling agent control.\n";
@@ -86,6 +88,8 @@ public:
 			return;
 		}
 		if(c == 'r'){
+			// override q-agent with continuous flaps
+			// q-table will continue to update
 			rapidFlap = 30;
 			return;
 		}
@@ -101,9 +105,10 @@ public:
 
 	void onMouseDown(int nButton, int x, int y)
 	{
-		//m_model.flap();
+		// use mouse button to pause view
 		if(*ssleep) *ssleep = false;
 		else *ssleep = true;
+		//m_model.flap();
 	}
 
 	void onMouseUp(int nButton, int x, int y)
@@ -119,50 +124,46 @@ public:
 	
 	string getState()
 	{
-		
 		string s = "";
-
-		// next tube state
+		// find next tube
 		if(m_model.tubes.size() > 0){
 		  int i = 0;
 		  while(m_model.tubes[i]->x < m_model.bird.x){
 			i++;
 		  }
+		  // next tube up/down
 		  s += to_string(m_model.tubes[i]->up);
-		  /*if(m_model.tubes[i]->up){
-		  	s += to_string((m_model.bird.y - m_model.tubes[i]->y) / discretizer);
-		  }else	s += to_string((m_model.tubes[i]->y - m_model.bird.y) / discretizer);*/
+		  // difference between bird-y and tube-y
 		  s += "," + to_string((m_model.bird.y - m_model.tubes[i]->y) / discretizer);
+		  // difference between bird-x and tube-x
 		  s += "," + to_string((m_model.tubes[i]->x - m_model.bird.x) / discretizer);
-		  
-		//  std::cout << "m_model.tubes[i]->y = " << m_model.tubes[i]->y << "\n";
 		}
 		else s += "0,0," + to_string(400 / discretizer);
 
-		// bird state
-		//s += "," + to_string(m_model.bird.y / discretizer);
+		// bird-y velocity
 		s += "," + to_string(m_model.bird.vert_vel / discretizer);
-		//std::cout << s << "\n";
 		return s;
 	}
 	
 	double getQvalue(bool flap, string state){
+		// lookup q-value, return 0.0 if none found
 		string s;
 		s = to_string(flap);
 		s += "," + state;
 		map<string,double>::iterator it = qtable->find(s);
 		double qvalue = 0.0;
 		if(it != qtable->end())
-			//qvalue = qtable.find(s)->second;
 			qvalue = it->second;
 		return qvalue;
 	}
 
 	void setQvalue(bool flap, bool reward){
-		// combine last action with previous state and current reward
+		// combine last action with previous state
 		string s;
 		s = to_string(flap);
 		s += "," + previousState;
+		
+		// current reward
 		double qvalue = 0.0;
 		if(reward)
 			qvalue = 1.0;
@@ -170,18 +171,18 @@ public:
 			//qvalue = 0.01;
 		if(!reward)
 			qvalue = -1000.0;
-		// Broken-Down Q-Learning Formula
-		//double q_j_flap = getQvalue(true, getState());
-		//double q_j_noflap = getQvalue(false, getState());
-		
-		//qvalue += (discount * max(q_j_flap, q_j_noflap));
+
+		// add discounted max next state
 		qvalue += (discount * std::max(getQvalue(true, getState()), getQvalue(false, getState())));
-		//qtable->erase(std::pair<string,double>(s, qvalue));
+		
+		// update qvalue or insert new
 		map<string,double>::iterator it = qtable->find(s);
 		if(it != qtable->end())
 			it->second = qvalue;
 		else
 			qtable->insert(std::pair<string,double>(s, qvalue));
+			
+		// debug
 		if(*ssleep) std::cout << "\t setq: " << qvalue << " = " << s << "\n";
 	}
 	
