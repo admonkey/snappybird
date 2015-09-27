007c03bcdbafd6b0817011fee23f6ca2555b1561
Jeff <jeff@jeffpuckett.com>
2015-09-27 16:47:03 -0500

    Document reinforcement learning algorithm

--- src/Agent.cpp
+++ src/Agent.cpp
@@ -6,6 +6,19 @@
   to the public domain (http://creativecommons.org/publicdomain/zero/1.0/).
 */
 
+/* 
+  Reinforcement Learning Algorithm:
+  Q(i, a) ← (1 − L^k)Q(i, a) + L^k [ r(i, a, j) + (γ * max(b∈A(j)) * Q(j, b)) ]
+  Q = Quantity of value of action given state
+  L^k = learning rate
+  i,j = state
+  a,b = action
+  A = set of possible actions a
+  r = reward
+  γ = discount factor 
+  	note: γ should decay at rate of e^(−µt(i,a,j))
+*/ 
+
 #include "Agent.h"
 
 Agent::Agent()
